9409d28a2ceba16ea651c44c466fe8c13eb48aed
Abhirup <argang05@gmail.com>
2025-04-13 20:16:54 +0530

    Try to make Microsoft Teams Plugin Compatible

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,24 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  // Make sure headers allow embedding in Microsoft Teams (iframe)
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'ALLOWALL', // Important: allow iframe embedding in Teams
+          },
+          {
+            key: 'Content-Security-Policy',
+            value: "frame-ancestors 'self' https://*.teams.microsoft.com https://teams.microsoft.com;", // Embed policy
+          },
+        ],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
